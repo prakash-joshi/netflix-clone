8322d09995208493ab875271cb1c8743fec9bd5f
prakash-joshi <prakash.joshi167@hotmail.com>
2024-03-23 21:37:42 +0530

    configure router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Body from "./components/Body";
+
 function App() {
-  return <div className="text-5xl  text-red-600 ">netflix clone</div>;
+  return (
+    <div className="text-5xl  text-red-600 ">
+      <Body />
+    </div>
+  );
 }
 
 export default App;
