5d7893b8808edf600c2aeb6961990978b02a6c94
prakash-joshi <prakash.joshi167@hotmail.com>
2024-03-25 17:42:43 +0530

    add user signout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,42 @@
+import { useDispatch, useSelector } from "react-redux";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { removeUser } from "../store/userSlice";
+
 const Header = () => {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(removeUser());
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log("error : ", error.message);
+      });
+  };
   return (
-    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-20">
+    <div className="absolute flex justify-between w-full px-8 py-2 bg-gradient-to-b from-black z-20">
       <img
         className="w-44"
         alt="logo"
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
       />
+      {user && (
+        <div className="flex">
+          <img className="w-16 h-16" alt="useriamge" src={user?.photoURL} />
+          <button
+            className="bg-red-700 text-white rounded-xl p-2 h-fit mt-4"
+            onClick={handleSignOut}
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
