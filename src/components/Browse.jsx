517461f764d4373e974641fe298a806048de18f4
prakash-joshi <prakash.joshi167@hotmail.com>
2024-03-27 13:14:37 +0530

    add TMDB now playing api and configure store for movies

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+import { API_MOVIE_NOW_PLAYING, API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../store/moviesSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(API_MOVIE_NOW_PLAYING, API_OPTIONS);
+    const json = await data.json();
+    console.log("json :", json);
+    dispatch(addNowPlayingMovies(json.results));
+  };
+
   return (
     <div>
       <Header />
